1c2b9756eb97847d8a4023cfd2e9ada82798264a
Gustavo Sabel <gustavo.sabel.gs@gmail.com>
2022-02-20 19:00:32 -0300

    Changed tests do async

--- api/src/tests/integration/hello_word.test.ts
+++ api/src/tests/integration/hello_word.test.ts
@@ -2,13 +2,9 @@ import { appPromise } from '../../app';
 import { chai, expect } from '../test_helper';
 
 describe('Hello World', () => {
-  it('Hello World', (done) => {
-    chai.request(appPromise)
-      .get('/')
-      .end((_, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({ title: 'Hello World! 👋' });
-        done();
-      });
+  it('Hello World', async () => {
+    const res = await chai.request(appPromise).get('/')
+    expect(res).to.have.status(200);
+    expect(res.body).to.deep.equal({ title: 'Hello World! 👋' });
   });
 });
--- api/src/tests/integration/player.test.ts
+++ api/src/tests/integration/player.test.ts
@@ -1,19 +1,14 @@
 import { appPromise } from '../../app'
 import { chai, expect } from '../test_helper';
 
-// https://blog.logrocket.com/unit-and-integration-testing-for-node-js-apps/
-// https://mochajs.org/#getting-started
 describe('Player', function () {
-  it('should create a new player', function (done) {
-    chai
+  it('should create a new player', async function () {
+    const res = await chai
       .request(appPromise)
       .post('/player')
-      .send({ name: 'Joao' })
-      .end((_, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body.name).to.be.equal('Joao')
-        expect(res.body.id).to.be.not.null
-        done();
-      });
+      .send({ name: 'Joao' });
+    expect(res).to.have.status(200);
+    expect(res.body.name).to.be.equal('Joao')
+    expect(res.body.id).to.be.not.null
   });
 });
