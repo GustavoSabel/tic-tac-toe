44a8bf18d11cf69fad81b214d998d45839f1d4ba
Gustavo Sabel <gustavo.sabel.gs@gmail.com>
2022-04-03 10:53:24 -0300

    Created more tests to player

--- tic-tac-toe-back/test/integration/player.test.ts
+++ tic-tac-toe-back/test/integration/player.test.ts
@@ -24,4 +24,21 @@ describe('Player', function () {
       .post('/player');
     expect(res).to.have.status(400);
   });
+  
+  it('should fail when the name is null', async function () {
+    const res = await chai
+      .request(app)
+      .post('/player')
+      .send({ name: null });
+    expect(res).to.have.status(400);
+  });
+  
+  // TODO: implement
+  it.skip('should fail when the name is empty', async function () {
+    const res = await chai
+      .request(app)
+      .post('/player')
+      .send({ name: '  ' });
+    expect(res).to.have.status(400);
+  });
 });
